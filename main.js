54fd1dde122a6f83411acf7ed5e77e6180e15948
inanimate631 <105974865+inanimate631@users.noreply.github.com>
2022-08-19 15:56:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const congratulation = document.querySelector('.congratulation')
 
 document.addEventListener('keydown', (e) => {
     if (e.keyCode === 32){
-        congratulation.style.transform = `translateX(-110%)`
+        congratulation.style.transform = `translateX(-140%)`
     }
 })
